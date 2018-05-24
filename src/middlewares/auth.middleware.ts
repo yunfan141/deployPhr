99d454d4116686537058cd36e964006e666eabb6
yunfan zhou <yunfanzhou@fq-wifi-10-215-207-134.wireless.pitt.edu>
2018-05-24 17:44:00 -0400

    auth middleware sucess

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -6,8 +6,8 @@ export class AuthMiddleware implements NestMiddleware {
   resolve(...args: any[]): ExpressMiddleware {
     return (req, res, next) => {
       console.log(req.headers);
-      if (req.headers.authorization && (req.headers.authorization as string).split(' ')[0] === 'Bearer'){
-        const token = (req.headers.authorization as string).split(' ')[1];
+      if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer'){
+        const token = req.headers.authorization.split(' ')[1];
         const decoded = jwt.verify(token, 'myprecious', {algorithm: 'HS384'});
         console.log(decoded);
         req.params.id = decoded.user_id;
