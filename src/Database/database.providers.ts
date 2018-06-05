c8f0ae76b2defe060f6e2c6d73864f4e3fbe0b7f
yunfan zhou <yunfanzhou@cl2-wifi-10-215-41-243.wireless.pitt.edu>
2018-06-05 10:41:20 -0400

    modified database configure

--- src/Database/database.providers.ts
+++ src/Database/database.providers.ts
@@ -5,11 +5,11 @@ export const databaseProviders = [
         provide: 'TypeORMInstance',
         useFactory: async () => await createConnection({
             type: 'postgres',
-            host: 'localhost',
+            host: 'ec2-23-23-226-190.compute-1.amazonaws.com',
             port: 5432,
-            username: 'postgres',
-            password: 'zyf1994',
-            database: 'mytest6',
+            username: 'meocnzjebzvbsw',
+            password: '0ba7f313427839e71cf5bafffdd9d5ab40f06495f1ab7dbe54713c9bce5c6661',
+            database: 'd7093s7ahmkbja',
             entities: [
                 __dirname + '/../*/*.entity{.ts,.js}',
                 'src/User/Users.entity.ts',
