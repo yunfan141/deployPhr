371514904df21accb186ba55c7eb776df3e6fa0e
yunfan zhou <yunfanzhou@cl2-wifi-10-215-37-252.wireless.pitt.edu>
2018-06-21 15:14:56 -0400

    database username erro

--- src/Database/database.providers.ts
+++ src/Database/database.providers.ts
@@ -27,7 +27,7 @@ export const databaseProviders = [
             type: 'postgres',
             host: 'ec2-23-23-226-190.compute-1.amazonaws.com',
             port: 5432,
-            username: 'postgres',
+            username: 'meocnzjebzvbsw',
             password: '0ba7f313427839e71cf5bafffdd9d5ab40f06495f1ab7dbe54713c9bce5c6661',
             database: 'd7093s7ahmkbja',
             entities: [
