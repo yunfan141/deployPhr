e2a8a203fafdcd8cb5745f321d00d4a9ca38d599
yunfan zhou <yunfanzhou@yunfans-MacBook-Pro.local>
2018-08-14 23:01:06 -0400

    deploy version

--- src/Database/database.providers.ts
+++ src/Database/database.providers.ts
@@ -20,6 +20,25 @@ import { createConnection } from 'typeorm';
 //     },
 // ];
 
+// export const databaseProviders = [
+//     {
+//         provide: 'TypeORMInstance',
+//         useFactory: async () => await createConnection({
+//             type: 'postgres',
+//             host: 'db',
+//             port: 5432,
+//             username: 'postgres',
+//             password: '12345',
+//             database: 'phr',
+//             entities: [
+//                 __dirname + '/../**/*.entity{.ts,.js}',
+//             ],
+//             logging: true,
+//             synchronize: true,
+//         }),
+//     },
+// ];
+
 export const databaseProviders = [
     {
         provide: 'TypeORMInstance',
